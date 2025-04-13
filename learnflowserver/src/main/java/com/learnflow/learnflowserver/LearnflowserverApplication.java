4b8e233888cda80df140c1b6a15460fcd5e090fb
bykbyk0401 <bykbyk0401@gmail.com>
2025-04-13 16:39:41 +0900

    init: 프로젝트 초기세팅

---
+++ learnflowserver/src/main/java/com/learnflow/learnflowserver/LearnflowserverApplication.java
@@ -0,0 +1,13 @@
+package com.learnflow.learnflowserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnflowserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnflowserverApplication.class, args);
+	}
+
+}
---
+++ learnflowserver/src/test/java/com/learnflow/learnflowserver/LearnflowserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnflow.learnflowserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnflowserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
